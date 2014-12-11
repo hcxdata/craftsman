cf5702f392978d55c7047e6ca36fdc98edf32eb7
seraph115 <seraph115@gmail.com>
2014-12-11 21:08:22 +0800

    添加了HomeController的Test

---
+++ craftsman-platform/src/test/java/com/bigbata/craftsman/web/HomeControllerTest.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.bigbata.craftsman.web;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+
+/** 
+ * 类说明:<br> 
+ * 创建时间: 2014年12月11日 下午8:49:50<br> 
+ * @author 刘岩松<br> 
+ * @email yansong.lau@gmail.com<br>  
+ */
+public class HomeControllerTest {
+	
+	@Test
+	public void testHomePage() throws Exception {
+		
+		HomeController controller = new HomeController();
+		MockMvc mockMvc = standaloneSetup(controller).build();
+		
+		mockMvc.perform(get("/")).andExpect(view().name("home"));
+	}
+
+}
