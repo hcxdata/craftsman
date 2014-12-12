d350d8d4568ad202483bac9fb6d58d69a9e984e0
seraph115 <seraph115@gmail.com>
2014-12-12 18:20:53 +0800

    添加了JPAConfig配置类

---
+++ craftsman-platform/src/main/java/com/bigbata/craftsman/config/JPAConfig.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.bigbata.craftsman.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/** 
+ * 类说明:<br> 
+ * 创建时间: 2014年12月12日 下午6:05:36<br> 
+ * @author 刘岩松<br> 
+ * @email yansong.lau@gmail.com<br>  
+ */
+@Configuration
+@EnableTransactionManagement
+public class JPAConfig {
+
+	
+	
+}
