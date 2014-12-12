448090fc8fa89c807e76c21383d77b9b5f427b1d
seraph115 <seraph115@gmail.com>
2014-12-12 16:11:35 +0800

    添加Spring Security配置类

---
+++ craftsman-platform/src/main/java/com/bigbata/craftsman/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.bigbata.craftsman.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+/** 
+ * 类说明:<br> 
+ * 创建时间: 2014年12月12日 下午3:44:11<br> 
+ * @author 刘岩松<br> 
+ * @email yansong.lau@gmail.com<br>  
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+}
