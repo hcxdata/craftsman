ad63644c09904605335811fb6f039a7f12385101
seraph115 <seraph115@gmail.com>
2015-01-01 21:18:29 +0800

    添加SmsMessages示例应用

--- craftsman-platform/src/main/java/com/bigbata/craftsman/web/HomeController.java
+++ craftsman-platform/src/main/java/com/bigbata/craftsman/web/HomeController.java
@@ -3,15 +3,9 @@
  */
 package com.bigbata.craftsman.web;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.bigbata.craftsman.dao.model.SmsMessage;
-import com.bigbata.craftsman.service.SmsMessageService;
 
 /** 
  * 类说明:<br> 
@@ -23,22 +17,9 @@ import com.bigbata.craftsman.service.SmsMessageService;
 @RequestMapping({"/", "/homepage"})
 public class HomeController {
 
-	private SmsMessageService smsMessageService;
-	
 	@RequestMapping(method = RequestMethod.GET)
 	public String home() {
-		
-		List<SmsMessage> smsMessages = smsMessageService.fetchAllSmsMessages();
-		
-		
-		
-		System.out.println(smsMessages.size());
 		return "home";
-	}
-
-	@Autowired
-	public void setSmsMessageService(SmsMessageService smsMessageService) {
-		this.smsMessageService = smsMessageService;
 	}
 
 }
