32270e231cb72d16fb9b0f98666068f73460aa8b
lixianghui <hui_ease@163.com>
2015-03-02 17:33:37 +0800

    添加异常错误展示功能

--- craftsman-webapp/src/main/webapp/app/system/users/app.js
+++ craftsman-webapp/src/main/webapp/app/system/users/app.js
@@ -3,6 +3,15 @@ angular.module(
     'app',
     ['pasvaz.bindonce', 'ngRoute', 'ngResource', 'app.route', 'app.controllers',
         'app.services', 'main.services', 'main.directives']).config(function ($httpProvider) {
+        function notifyError(rejection){
+            $.bigBox({
+                title: rejection.status + ' ' + rejection.statusText,
+                content: rejection.data.message,
+                color: "#C46A69",
+                icon: "fa fa-warning shake animated",
+                timeout: 6000
+            });
+        }
         $httpProvider.interceptors.push(function ($q) {
             return {
                 request: function (config) {
@@ -12,6 +21,8 @@ angular.module(
                 responseError: function (res) {
                     if (res.status === 401) {
                         location.reload();
+                    }else if(res.status === 500){
+                        notifyError(res);
                     }
                     return $q.reject(res);
                 }
