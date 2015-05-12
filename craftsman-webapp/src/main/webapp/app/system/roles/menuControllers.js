374dbfbf9a2c7a967340ad401abedf971ed640a6
huanghaiyang <huanhai24@sina.com>
2015-05-12 15:50:06 +0800

    角色菜单功能

--- craftsman-webapp/src/main/webapp/app/system/roles/menuControllers.js
+++ craftsman-webapp/src/main/webapp/app/system/roles/menuControllers.js
@@ -16,6 +16,8 @@ angular.module('controllers').controller('IndexMenuController',
                 });
             else
                 $scope.tree_data = [];
+
+            expanded_all = false;
         };
         /*列定义*/
         $scope.col_defs = [{
@@ -26,20 +28,48 @@ angular.module('controllers').controller('IndexMenuController',
             displayName: "是否叶子节点",
             cellTemplate: '<text ng-if="row.branch[col.field] === true">是</text><text ng-if="row.branch[col.field] === false">否</text>'
         }];
-
+        // 树控制器
         $scope.my_tree = {};
-
-        $scope.expand_branch_all = function(){
-            $scope.my_tree.expand_all();
+        // 初始化还未展开
+        var expanded_all = false;
+        // 当treeData数据发生变化，就会调用次函数
+        $scope.expand_branch_all = function() {
+            if (expanded_all === false) {
+                // 展开所有节点
+                $scope.my_tree.expand_all();
+                // 已展开完成
+                expanded_all = true;
+            }
         };
 
         $scope.refresh();
-    }).controller('EditController',
-    function($scope, $location, $routeParams, menuService) {
+
         $scope.save = function() {
-            menuService.update($scope.menu, function() {
+            var data = {
+                id: $scope.role.id,
+                menus: (function() {
+                    function d(data) {
+                        if (data.checked === true) {
+                            ids.push(data.id);
+                            if (data.children && data.children.length > 0) {
+                                for (var i = 0; i < data.children.length; i++) {
+                                    d(data.children[i]);
+                                }
+                            }
+                        }
+                    };
+                    var ids = [];
+                    var treeData = $scope.my_tree.get_treeData();
+                    if (treeData.length > 0) {
+                        for (var i = 0; i < treeData.length; i++) {
+                            d(treeData[i]);
+                        }
+                    }
+                    return ids;
+                }())
+            };
+            menuService.update(data, function() {
                 Fun.msg.notifyInfo();
-                $location.url("/index?initChain=" + $scope.initChain);
             });
         };
     });
--- craftsman-webapp/src/main/webapp/app/system/roles/roleControllers.js
+++ craftsman-webapp/src/main/webapp/app/system/roles/roleControllers.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('controllers').controller('IndexRoleController',
-    function ($scope, $location, roleService, Paginator, $routeParams) {
+    function($scope, $location, roleService, Paginator, $routeParams) {
         $scope.page = Paginator({
             resource: roleService,
-            successCallback: function (data) {
+            successCallback: function(data) {
                 if ($routeParams.id) {
                     var Role = null;
                     for (var i in data.content) {
@@ -15,55 +15,60 @@ angular.module('controllers').controller('IndexRoleController',
                 }
             }
         });
-        $scope.reset = function () {
+        $scope.reset = function() {
             $scope.name = "";
             $scope.search();
+            $scope.$emit("refreshAndShowMenuList", {});
+            $scope.$emit("getRole", {});
         };
-        $scope.search = function () {
+        $scope.search = function() {
             $scope.page.setParams({
                 name: $scope.name
             });
             $scope.page.fresh();
+
+            $scope.$emit("refreshAndShowMenuList", {});
+            $scope.$emit("getRole", {});
         };
-        $scope.showMenuList = function (index, role) {
+        $scope.showMenuList = function(index, role) {
             $scope.$emit("refreshAndShowMenuList", role);
             $scope.$emit("getRole", role);
         }
 
-        $scope.delete = function (id) {
-            Fun.msg.delConfirm(function () {
+        $scope.delete = function(id) {
+            Fun.msg.delConfirm(function() {
                 $location.url("/" + id + "/del");
                 $scope.$apply();
             });
         };
 
     }).controller('NewRoleController',
-    function ($scope, $location, roleService) {
-        $scope.save = function () {
-            roleService.save($scope.role, function () {
+    function($scope, $location, roleService) {
+        $scope.save = function() {
+            roleService.save($scope.role, function() {
                 Fun.msg.notifyInfo();
                 $location.path("/index");
             });
         }
     }).controller('EditRoleController',
-    function ($scope, $location, $routeParams, roleService) {
+    function($scope, $location, $routeParams, roleService) {
         roleService.get({
             id: $routeParams.id
-        }, function (role) {
+        }, function(role) {
             $scope.role = role;
         });
-        $scope.save = function () {
-            roleService.update($scope.role, function () {
+        $scope.save = function() {
+            roleService.update($scope.role, function() {
                 Fun.msg.notifyInfo();
                 $location.path("/" + $scope.role.id + "/index");
             });
         }
 
     }).controller('DelRoleController',
-    function ($scope, $location, $routeParams, roleService) {
+    function($scope, $location, $routeParams, roleService) {
         roleService.delete({
             id: $routeParams.id
-        }, function () {
+        }, function() {
             Fun.msg.notifyInfo();
             $location.path("/index");
         });
